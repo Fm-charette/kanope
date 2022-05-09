b971a176440c10e4805d1f2fba82b8cc53e9bc8a
flo@MacBook-Pro-De-FLoryan <floryan.mollet@epitech.eu>
2022-05-09 15:32:49 +0200

    Login and register component added to login page, add /login route to route.js . Add url api to axios.js

---
+++ front/src/components/Login/registerForm.vue
@@ -0,0 +1,66 @@
+<template>
+  <q-form @submit.prevent="submitForm">
+
+    <q-input
+      outlined
+      v-model="form.email"
+      label="E-mail"
+      class="q-my-md"
+      :rules="[val => validateEmail(val) || 'Email invalide']"
+    />
+
+    <q-input
+      type="password"
+      outlined
+      v-model="form.password"
+      label="Mot de passe"
+      class="q-my-md"
+      :rules="[ val => val.length >= 4 || 'Minimum 4 caractère']"
+      lazy-rules
+    />
+
+    <q-input
+      type="password"
+      outlined
+      v-model="form.password_confirmation"
+      label="Confirmer le mot de passe"
+      class="q-my-md"
+      :rules="[ val => val === form.password || 'Les mots de passe sont différents']"
+      lazy-rules
+    />
+
+    <q-btn
+      type="submit"
+      color="primary"
+      label="Créer un compte"
+    />
+  </q-form>
+</template>
+
+<script>
+export default {
+  name: 'registerForm',
+  data () {
+    return {
+      form: {
+        email: '',
+        password: '',
+        password_confirmation: ''
+      }
+    }
+  },
+  methods: {
+    submitForm () {
+      alert('Formulaire envoyé !')
+    },
+    validateEmail (email) {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return re.test(String(email).toLowerCase())
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ front/src/pages/myLogin.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-page padding>
+    <q-card class="login">
+      <q-tabs
+        v-model="tab"
+        class="text-grey"
+        active-color="primary"
+        indicator-color="primary"
+        align="justify"
+        narrow-indicator
+      >
+        <q-tab name="login" label="Se connecter" />
+        <q-tab name="register" label="Créer un compte" />
+      </q-tabs>
+
+      <q-separator />
+
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="login">
+          <cnx-form/>
+        </q-tab-panel>
+
+        <q-tab-panel name="register">
+          <cnx-formRegister/>
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'myLogin',
+  data () {
+    return {
+      tab: 'login'
+    }
+  },
+  components: {
+    'cnx-form': require('components/Login/loginForm').default,
+    'cnx-formRegister': require('components/Login/registerForm').default
+  }
+}
+</script>
+
+<style scoped>
+  .login {
+    max-width: 500px;
+    width: auto;
+    margin: 0 auto;
+  }
+</style>
